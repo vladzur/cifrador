e59b06c4665f199954c520b8679d90a8a97cad01
vladzur <vladzur@gmail.com>
2015-12-07 11:14:01 -0300

    PSR-4 namespace

--- Cifrador.php
+++ Cifrador.php
@@ -1,4 +1,4 @@
-<?php
+<?php namespace Vladzur\Cifrador;
 /*
  *      Cifrador.php
  *
@@ -21,7 +21,8 @@
  *
  *
  */
-namespace Vladzur\Cifrador;
+
+
 /**
  * Clase para cifrar y descifrar textos utilizando la función mcryt en modo CFB
  * y el algoritmo Rijndael 256 (AES).
@@ -30,115 +31,124 @@ namespace Vladzur\Cifrador;
  *
  * @author Vladimir Zurita
  */
-class Cifrador {
+class Cifrador
+{
 
-	private $clave;
-	private $iv;
+    private $clave;
+    private $iv;
 
-	/**
-	 * Alias for Cifrador::cifrar()
-	 * @param $text
-	 * @param $password
-	 * @return string
-	 */
-	public function encrypt($text, $password) {
-		return $this->cifrar($text, $password);
-	}
+    /**
+     * Alias for Cifrador::cifrar()
+     * @param $text
+     * @param $password
+     * @return string
+     */
+    public function encrypt($text, $password)
+    {
+        return $this->cifrar($text, $password);
+    }
 
-	/**
-	 * Cifra un texto utilizando el algoritmo Rijndael_256 (AES), retornando el texto
-	 * como binario o en base 64.
-	 *
-	 * @param string $texto Texto a cifrar
-	 * @param string $password Contraseña para cifrar
-	 * @return string Texto cifrado
-	 */
-	public function cifrar($texto, $password) {
-		$this->generarClave($password);
-		if (empty($this->iv)) {
-			$this->generarIV();
-		}
-		$resultado = $this->iv . mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $this->clave, $texto, MCRYPT_MODE_CFB, $this->iv);
-		return base64_encode($resultado);
-	}
+    /**
+     * Cifra un texto utilizando el algoritmo Rijndael_256 (AES), retornando el texto
+     * como binario o en base 64.
+     *
+     * @param string $texto Texto a cifrar
+     * @param string $password Contraseña para cifrar
+     * @return string Texto cifrado
+     */
+    public function cifrar($texto, $password)
+    {
+        $this->generarClave($password);
+        if (empty($this->iv)) {
+            $this->generarIV();
+        }
+        $resultado = $this->iv . mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $this->clave, $texto, MCRYPT_MODE_CFB, $this->iv);
+        return base64_encode($resultado);
+    }
 
-	/**
-	 * Genera una clave binaria de la contraseña entregada usando el algoritmo SHA256.
-	 *
-	 * @param string $password Contraseña para generar la clave
-	 */
-	private function generarClave($password) {
-		$this->clave = hash('SHA256', $password, true);
-	}
+    /**
+     * Genera una clave binaria de la contraseña entregada usando el algoritmo SHA256.
+     *
+     * @param string $password Contraseña para generar la clave
+     */
+    private function generarClave($password)
+    {
+        $this->clave = hash('SHA256', $password, true);
+    }
 
-	/**
-	 * Genera el vector de inicialización (IV) para el algoritmo Rijndael_256 (AES).
-	 */
-	private function generarIV() {
-		$this->iv = mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CFB), MCRYPT_DEV_URANDOM);
-	}
+    /**
+     * Genera el vector de inicialización (IV) para el algoritmo Rijndael_256 (AES).
+     */
+    private function generarIV()
+    {
+        $this->iv = mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_CFB), MCRYPT_DEV_URANDOM);
+    }
 
-	/**
-	 * Alias for Cifrador::descifrar()
-	 * @param $text
-	 * @param $password
-	 * @return string
-	 */
-	public function decrypt($text, $password) {
-		return $this->descifrar($text, $password);
-	}
+    /**
+     * Alias for Cifrador::descifrar()
+     * @param $text
+     * @param $password
+     * @return string
+     */
+    public function decrypt($text, $password)
+    {
+        return $this->descifrar($text, $password);
+    }
 
-	/**
-	 * Descifra un texto utilizando el algoritmo Rijndael_256 (AES) retornando el texto en claro.
-	 *
-	 * @param string $text Texto a descifrar, puede estar en binario o base 64
-	 * @param string $password La contraseña que se usó para cifrar
-	 * @param bool $base64 Indica si el texto a descifrar está en base 64 o no
-	 * @return string Texto descifrado.
-	 */
-	public function descifrar($texto, $password) {
-		$this->generarClave($password);
-		$resultado = base64_decode($texto);
-		$iv = substr($resultado, 0, 32);
-		$cifrado = substr($resultado, 32);
-		$descifrado = mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $this->clave, $cifrado, MCRYPT_MODE_CFB, $iv);
-		return $descifrado;
-	}
+    /**
+     * Descifra un texto utilizando el algoritmo Rijndael_256 (AES) retornando el texto en claro.
+     *
+     * @param string $text Texto a descifrar, puede estar en binario o base 64
+     * @param string $password La contraseña que se usó para cifrar
+     * @param bool $base64 Indica si el texto a descifrar está en base 64 o no
+     * @return string Texto descifrado.
+     */
+    public function descifrar($texto, $password)
+    {
+        $this->generarClave($password);
+        $resultado = base64_decode($texto);
+        $iv = substr($resultado, 0, 32);
+        $cifrado = substr($resultado, 32);
+        $descifrado = mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $this->clave, $cifrado, MCRYPT_MODE_CFB, $iv);
+        return $descifrado;
+    }
 
-	/**
-	 * Genera una contraseña aleatoria de largo indicado.
-	 * Basado en ejemplo mt_rand() en php.net
-	 *
-	 * @param int $largo Cantidad de caracteres de la contraseña default 16.
-	 * @return string Contraseña generada.
-	 */
-	public function generarPassword($largo = 16) {
-		$password = '';
-		for ($i = 0; $i < $largo; ++$i) {
-			if ($i % 2 == 0) {
-				mt_srand(time() % 2147 * 1000000 + (double)microtime() * 1000000);
-			}
-			$rnd = mt_rand(48, 122);
-			if ($rnd > 57 && $rnd < 65) {
-				$rnd += 6;
-			}
-			if ($rnd > 90 && $rnd < 97) {
-				$rnd += 7;
-			}
-			$password .= chr($rnd);
-		}
-		return $password;
-	}
+    /**
+     * Genera una contraseña aleatoria de largo indicado.
+     * Basado en ejemplo mt_rand() en php.net
+     *
+     * @param int $largo Cantidad de caracteres de la contraseña default 16.
+     * @return string Contraseña generada.
+     */
+    public function generarPassword($largo = 16)
+    {
+        $password = '';
+        for ($i = 0; $i < $largo; ++$i) {
+            if ($i % 2 == 0) {
+                mt_srand(time() % 2147 * 1000000 + (double)microtime() * 1000000);
+            }
+            $rnd = mt_rand(48, 122);
+            if ($rnd > 57 && $rnd < 65) {
+                $rnd += 6;
+            }
+            if ($rnd > 90 && $rnd < 97) {
+                $rnd += 7;
+            }
+            $password .= chr($rnd);
+        }
+        return $password;
+    }
 
-	/**
-	 * Genera un hash usando el algoritmo SHA1.
-	 *
-	 * @param string $texto Texto para generar hash
-	 * @param bool $raw Tipo de hash binario (true) o base64 (false)
-	 * @return string Hash
-	 */
-	public function generarHash($texto, $raw = false) {
-		return hash('SHA1', $texto, $raw);
-	}
+    /**
+     * Genera un hash usando el algoritmo SHA1.
+     *
+     * @param string $texto Texto para generar hash
+     * @param bool $raw Tipo de hash binario (true) o base64 (false)
+     * @return string Hash
+     */
+    public function generarHash($texto, $raw = false)
+    {
+        return hash('SHA1', $texto, $raw);
+    }
 
 }
